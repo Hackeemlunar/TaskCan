8388114f4fb3cbb4442acec48dc6677e047fe83d
Hackeem Mensah Bosu <66512968+Hackeemlunar@users.noreply.github.com>
2025-04-09 15:34:03 +0400

    Made changes I dont remember

--- src/main/java/me/aceking/users/UserController.java
+++ src/main/java/me/aceking/users/UserController.java
@@ -1,6 +1,9 @@
 package me.aceking.users;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import me.aceking.tasks.ITaskService;
+import me.aceking.tasks.dtos.TaskResponseDto;
 import me.aceking.users.dto.CreateUserDto;
 import me.aceking.users.dto.UpdateUserDto;
 import me.aceking.users.dto.UserResponseDto;
@@ -9,53 +12,52 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/v1/users")
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/users")
 public class UserController {
     private final IUserService userService;
+    private final ITaskService taskService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
+
+    @GetMapping("/signup")
+    public String createUserForm(Model model) {
+        model.addAttribute("user", new CreateUserDto(null, null, null, null));
+        return "/users/newUser";
     }
 
-    @PostMapping("/")
-    public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody CreateUserDto user) {
-        return ResponseEntity.ok(userService.createUser(user));
+    @PostMapping("/signup")
+    public String createUser(@Valid @ModelAttribute("user") CreateUserDto user) {
+        userService.createUser(user);
+        return "redirect:/users";
     }
 
-    @GetMapping("/username/{username}")
-    public ResponseEntity<UserResponseDto> getUserByUsername(@PathVariable String username) {
-        return ResponseEntity.ok(userService.getUserByUsername(username));
+    @GetMapping("/{id}")
+    public String getUser(@PathVariable Long id, @RequestParam(defaultValue = "0") int page, Model model) {
+        UserResponseDto user = userService.getUserById(id);
+        model.addAttribute("user", user);
+        Pageable pageable = PageRequest.of(page, 10, Sort.by("id"));
+        Page<TaskResponseDto> tasks = taskService.getTasksByUserId(pageable, id);
+        model.addAttribute("tasks", tasks);
+
+        return "/users/user";
     }
 
-    @GetMapping("/id/{id}")
-    public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
-        return ResponseEntity.ok(userService.getUserById(id));
+    @PostMapping("/{id}/edit")
+    public String updateUser(@PathVariable Long id, @Valid @ModelAttribute("user") UpdateUserDto user) {
+        userService.updateUser(id, user);
+        return "redirect:/users";
     }
 
-    @GetMapping
-    public ResponseEntity<Page<UserResponseDto>> getUsers(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
-            @RequestParam(defaultValue = "id") String sortBy) {
-        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
-        return ResponseEntity.ok(userService.getUsers(pageable));
-    }
-
-    @PatchMapping("/{id}")
-    public ResponseEntity<UserResponseDto> updateUser(@PathVariable Long id, @Valid @RequestBody UpdateUserDto user) {
-        return ResponseEntity.ok(userService.updateUser(id, user));
-    }
-
-    @DeleteMapping("/id/{id}")
-    public void deleteUserById(@PathVariable Long id) {
+    @PostMapping("/{id}/delete")
+    public String deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
-    }
-
-    @DeleteMapping("/username/{username}")
-    public void deleteUserByUsername(@PathVariable String username) {
-        userService.deleteByUsername(username);
+        return "redirect:/users";
     }
 }
